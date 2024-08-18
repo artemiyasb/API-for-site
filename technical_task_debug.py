0b8394110cbc51212fb0fc46c5f6527daba35010
Artem <vtyurin.artem@bk.ru>
2024-08-18 20:13:21 +0500

    Update technical_task_debug.py

--- technical_task_debug.py
+++ technical_task_debug.py
@@ -1,15 +1,15 @@
 def user_authorization():
-    login = str(input('Enter your Login'))
-    password = str(input('Enter your Password'))
+    login = input('Enter your Login')
+    password = input('Enter your Password')
     with open('a.txt', 'r') as f:
         text = f.read().split('\n')
         for line in text:
             line = line.split()
             if line[0] == login and line[1] == password:
-                print('Found!')
-                print('Authorization completed!')
+                return 'Found!'
+                return 'Authorization completed!'
             else:
-                print('Login or password not found!')
+                return 'Login or password not found!'
 
 def write_to_file(login, password):
     with open('a.txt', 'w') as f:
@@ -17,22 +17,22 @@ def write_to_file(login, password):
         f.write(" " f'{password}')
 
 def user_registration():
-    login = str(input('Enter your Login'))
-    password = str(input('Enter your password'))
+    login = input('Enter your Login')
+    password = input('Enter your password')
     if len(login) >= 3 and len(login) <= 20 and len(password) >= 4 and len(password) <= 32:
         write_to_file(login, password)
-        print('Registration passed successful!')
+        return 'Registration passed successful!'
     else:
-        print('Registration error!')
+        return 'Registration error!'
 
 def user_start():
     user_choose = int(input('Type 1 for registration or 2 for authorization: '))
     if user_choose == 1:
-        print('Welcome to registration!')
+        return 'Welcome to registration!'
         user_registration()
     elif user_choose == 2:
-        print('Welcome to authorization!')
+        return 'Welcome to authorization!'
         user_authorization()
     else:
-        print('The invalid meaning!')
+        return 'The invalid meaning!'
 user_start()
